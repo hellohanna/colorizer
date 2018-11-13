a561519ff0b32de933aa0aba512d4b75443c7d69
Hanna Babushkina <anylike90@gmail.com>
2018-11-12 20:58:31 -0800

    Add nginx.conf, turn off debug mode

--- server.py
+++ server.py
@@ -292,7 +292,7 @@ def train_dataset(dataset_id):
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
     # that we invoke the DebugToolbarExtension
-    app.debug = True
+    app.debug = False
 
     connect_to_db(app)
     app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
